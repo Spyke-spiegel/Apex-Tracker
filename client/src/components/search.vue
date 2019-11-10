5dfe0f7830d64be25ec1e25999921e6c57ce7e8e
Florent Ayot <florent.ayot33@gmail.com>
2019-11-10 13:47:21 +0800

    readded search and header page

---
+++ client/src/components/search.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="search">
+    <h1>Track Player Stats</h1>
+    <form v.on:submit.prevent="onSubmit">
+      <div class="form-group">
+        <label for="platform">Platform</label>
+        <select name="platform" id="platform" v-model="platform">
+          <option value="psn"></option>
+          <option value="xbl"></option>
+          <option value="Origin"></option>
+        </select>
+      </div>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
